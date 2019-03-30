62bfced89e154cfd0ffd70f6f9d9c68b4e3b4f42
Roman2233 <mts90580@gmail.com>
2019-03-30 20:03:22 +0800

    Translator Redisign

---
+++ TRANSLATOR/java.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function() {
+    var btn = document.querySelector("#btn"),
+    txt = document.querySelector("#en"),
+    ru = document.querySelector("#ru") ;
+    btn.addEventListener("click", function() {
+
+var select = document.getElementById("sele");
+var val = select.value;
+
+
+var request = new XMLHttpRequest();
+var text = encodeURIComponent(txt.value);
+var key = "trnsl.1.1.20190204T134244Z.cbb90abf77f9ffce.4745335122996d2851888242018bbf3227717c77";
+var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key="+key+"&text="+text+"&lang="+val+"&format=plain&options=1"
+request.open('GET', url, true);
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText);
+    ru.value = data.text;
+  }
+};
+request.send();
+    });
+});
